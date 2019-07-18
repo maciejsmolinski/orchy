3b5e95e13743a73921f13203d6672041a579ee78
Maciej Smolinski <contact@maciejsmolinski.com>
2019-07-19 00:18:35 +0200

    chore(Main): remove temporary examples, execute commands from definition

--- src/Main.purs
+++ src/Main.purs
@@ -3,37 +3,13 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Aff (launchAff_)
-import Effect.Class (liftEffect)
-import Effect.Console (log)
 import Orchestrator.Main (Definition, makeCommand, makeDefinition, runDefinition)
-import System.Commands (asyncExec, syncExec)
 
 main :: Effect Unit
-main = do
-  log "Orchestrator.Main.runDefinition/start"
-  runDefinition definition
-  log "Orchestrator.Main.runDefinition/end"
-
-  log "---"
-
-  log "System.Commands.syncExec/start"
-  syncExec "pwd" [] >>= logOutput
-  log "System.Commands.syncExec/end"
-
-  log "---"
-
-  launchAff_ do
-    liftEffect $ log "System.Commands.asyncExec/start"
-    asyncExec "pwd" [] >>= (liftEffect <<< logOutput)
-    liftEffect $ log "System.Commands.asyncExec/end"
-
-  where
-    logOutput output = log $ "-> " <> (show output)
-
-
+main = runDefinition definition
 
 definition :: Definition
-definition = makeDefinition $ [ makeCommand "git" ["status"]
-                              , makeCommand "echo" ["\"done\""]
+definition = makeDefinition $ [ makeCommand "pwd" []
+                              , makeCommand "git" ["branch"]
+                              , makeCommand "git" ["status"]
                               ]
