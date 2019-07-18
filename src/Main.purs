2c6eae1fb592ea96c6795a77dd438276eb00cf4a
Maciej Smolinski <contact@maciejsmolinski.com>
2019-07-18 21:51:53 +0200

    chore(Main): run examples of `runDefinition`, `syncExec` and `asyncExec`

--- src/Main.purs
+++ src/Main.purs
@@ -3,14 +3,35 @@ module Main where
 import Prelude
 
 import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
 import Effect.Console (log)
 import Orchestrator.Main (Definition, makeCommand, makeDefinition, runDefinition)
+import System.Commands (asyncExec, syncExec)
 
 main :: Effect Unit
 main = do
-  log "Orchestration started"
+  log "Orchestrator.Main.runDefinition/start"
   runDefinition definition
-  log "Orchestration done"
+  log "Orchestrator.Main.runDefinition/end"
+
+  log "---"
+
+  log "System.Commands.syncExec/start"
+  syncExec "pwd" [] >>= logOutput
+  log "System.Commands.syncExec/end"
+
+  log "---"
+
+  launchAff_ do
+    liftEffect $ log "System.Commands.asyncExec/start"
+    asyncExec "pwd" [] >>= (liftEffect <<< logOutput)
+    liftEffect $ log "System.Commands.asyncExec/end"
+
+  where
+    logOutput output = log $ "-> " <> (show output)
+
+
 
 definition :: Definition
 definition = makeDefinition $ [ makeCommand "git" ["status"]
