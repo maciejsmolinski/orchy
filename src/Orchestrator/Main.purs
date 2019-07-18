4b30ecaa359156d4e133e806b12b490066d8ef77
Maciej Smolinski <contact@maciejsmolinski.com>
2019-07-19 00:18:08 +0200

    chore(Orchestrator): run definitions using `asyncExec`

--- src/Orchestrator/Main.purs
+++ src/Orchestrator/Main.purs
@@ -1,16 +1,25 @@
 module Orchestrator.Main (makeCommand, makeDefinition, runDefinition, Definition, Command) where
 
+
+
+import Control.Applicative (pure)
+import Control.Bind (bind, discard, (>>=))
+import Control.Monad.Error.Class (try)
 import Data.Array ((:))
+import Data.Either (Either(..))
 import Data.Foldable (foldl)
-import Data.Function (($), (<<<))
+import Data.Function (($))
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
 import Data.Semigroup ((<>))
-import Data.Show (class Show)
+import Data.Show (class Show, show)
 import Data.Traversable (traverse_)
-import Data.Unit (Unit)
+import Data.Unit (Unit, unit)
 import Effect (Effect)
+import Effect.Aff (Aff, launchAff_)
+import Effect.Class (liftEffect)
 import Effect.Console (log)
+import System.Commands (asyncExec)
 
 type Program = String
 type Args = Array String
@@ -37,13 +46,28 @@ makeDefinition :: Array Command -> Definition
 makeDefinition commands = Definition { commands }
 
 runDefinition :: Definition -> Effect Unit
-runDefinition (Definition { commands }) = traverse_ execute commands
+runDefinition (Definition { commands }) = execCommands commands
   where
-    print :: String -> Effect Unit
-    print = log <<< ((<>) "->")
-
-    execute :: Command -> Effect Unit
-    execute (Command program args) = print <<< showPretty $ program : args
-
     showPretty :: Array String -> String
     showPretty args = foldl (\a b -> a <> " " <> b) "" args
+
+    annotate :: Command -> Aff Command
+    annotate command@(Command program args) = do
+      liftEffect $ log $ "* Running" <> (showPretty (program : args))
+      pure command
+
+    run :: Command -> Aff (Either String String)
+    run (Command program args) = asyncExec program args
+
+    logOutput :: Either String String -> Aff Unit
+    logOutput value = liftEffect $ log $ "-> " <> (show value)
+
+    execCommands :: Array Command -> Effect Unit
+    execCommands items =
+      launchAff_ do
+        liftEffect $ log "Executing"
+        result <- try $ traverse_ (\command -> (annotate command) >>= run >>= logOutput) items
+        case result of
+          (Left _) -> (liftEffect $ log "Execution FAILED")
+          (Right _) -> (liftEffect $ log "Execution SUCCEEDED")
+        pure unit
