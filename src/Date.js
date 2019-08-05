ce1a086f032f6266168054d483847c4337d51366
Maciej Smolinski <contact@maciejsmolinski.com>
2019-08-05 23:50:51 +0200

    feat(Date): add extra methods `yyyymmdd` and `hhmmss`

--- src/Date.js
+++ src/Date.js
@@ -4,7 +4,10 @@ function date() {
     return [
         String(currentDate.getDate()).padStart(2, '0'),
         String(currentDate.getMonth() + 1).padStart(2, '0'),
-        String(currentDate.getFullYear())
+        String(currentDate.getFullYear()).padStart(4, '0'),
+        String(currentDate.getHours()).padStart(2, '0'),
+        String(currentDate.getMinutes()).padStart(2, '0'),
+        String(currentDate.getSeconds()).padStart(2, '0'),
     ];
 }
 
@@ -13,4 +16,16 @@ exports.ddmmyyyy = function() {
     var currentDate = date();
 
     return currentDate[0] + '/' + currentDate[1] + '/' + currentDate[2];
+}
+
+exports.yyyymmdd = function() {
+    var currentDate = date();
+
+    return currentDate[2] + '/' + currentDate[1] + '/' + currentDate[0];
+}
+
+exports.hhmmss = function() {
+    var currentDate = date();
+
+    return currentDate[3] + ':' + currentDate[4] + ':' + currentDate[5];
 }
--- src/Date.purs
+++ src/Date.purs
@@ -1,5 +1,9 @@
-module Date (ddmmyyyy) where
+module Date (ddmmyyyy, yyyymmdd, hhmmss) where
 
 import Effect (Effect)
 
 foreign import ddmmyyyy :: Effect String
+
+foreign import yyyymmdd :: Effect String
+
+foreign import hhmmss :: Effect String
